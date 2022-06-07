4f4efff462c93c5b2a64cd566ec3f5be6f806cda
StephensIriarte <stephens_iriarte@yahoo.com>
2022-06-07 00:46:27 -0400

    componentes-iriarte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponentComponent } from './components/nav-component/nav-component.component';
+import { AsideComponentComponent } from './components/aside-component/aside-component.component';
+import { MainComponentComponent } from './components/main-component/main-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponentComponent,
+    AsideComponentComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/nav-component/nav-component.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-component',
+  templateUrl: './nav-component.component.html',
+  styleUrls: ['./nav-component.component.css']
+})                                                                                            
+
+
+
+
+
+
+export class NavComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
